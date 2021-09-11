f4913f0c852fa6782348fc5b7346f401ee334f92
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2021-09-11 22:31:25 +0600

    script updated

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const sun = document.querySelector('.sun');
 const body = document.querySelector('body');
-sun.onclick = function () {
-    body.classList.toggle('dark')    
-}
+// sun.onclick = function () {
+//     body.classList.toggle('dark')    
+// }
+
+sun.addEventListener('click', () =>{
+    body.classList.toggle('dark')
+})
